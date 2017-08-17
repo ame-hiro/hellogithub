6893a3c38e964ddc1e88c0235127e15a995aa15b
ame-hiro <31101033+ame-hiro@users.noreply.github.com>
2017-08-17 22:37:26 +0900

    CommitTest

---
+++ Main.java
@@ -0,0 +1,11 @@
+﻿import java.util.*;
+class Main{
+    public static void main(String[]args){
+        Scanner cin=new Scanner(System.in);
+        String line;
+        for(;cin.hasNext();){
+            line=cin.nextLine();
+            System.out.println(line.toUpperCase());
+        }
+    }
+}
